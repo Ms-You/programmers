2b9f97f393ba2f7cc77fec44b84208e3254d7134
Myeongsu Yu <msu122005@hs.ac.kr>
2022-08-24 00:51:28 +0900

    숫자의 표현 수정

--- Lv.2/숫자의 표현/Solution.java
+++ Lv.2/숫자의 표현/Solution.java
@@ -15,3 +15,16 @@ class Solution {
         return cnt;
     }
 }
+
+// 간단하게 수정한 코드
+class Solution {
+    public int solution(int n) {
+        int cnt = 0;
+        for (int i = 1; i <= n; i++){
+            if (n % i == 0){
+                if (i % 2 != 0) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
