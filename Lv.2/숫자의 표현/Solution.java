4ce8815f15ec7386390181dce039d492f73460a4
Myeongsu Yu <msu122005@hs.ac.kr>
2022-08-24 00:46:48 +0900

    숫자의 표현 풀이

---
+++ Lv.2/숫자의 표현/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int cnt = 0;
+        if (n % 2 == 0){
+            for (int i = 1; i <= n; i++){
+                if (n % i == 0){
+                    if (i % 2 != 0) cnt++;
+                }
+            }
+        }
+        else {
+            for (int i = 1; i <= n; i++)
+                if (n % i == 0) cnt++;
+        }
+        return cnt;
+    }
+}
