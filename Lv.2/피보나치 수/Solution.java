914528e7b7c5328ee203ce81926310872f7eec3d
Myeongsu Yu <msu122005@hs.ac.kr>
2022-08-24 00:06:21 +0900

    피보나치 수 풀이

---
+++ Lv.2/피보나치 수/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+        
+        int a = 0, b = 1, c = 0;
+        for (int i = 2; i <= n; i++){
+            c = (a + b) % 1234567;
+            a = b;
+            b = c;
+        }
+        
+        return c;
+    }
+}
