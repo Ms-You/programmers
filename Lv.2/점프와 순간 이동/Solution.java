ca1f69342a6ec4405d12e81c9b3e9aa6863c6950
Myeongsu Yu <msu122005@hs.ac.kr>
2022-09-16 17:36:54 +0900

    점프와 순간 이동 풀이

---
+++ Lv.2/점프와 순간 이동/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n != 0){
+            if (n % 2 != 0) {
+                n -= 1;
+                answer++;
+            }
+            else n /= 2;
+        }
+        return answer;
+    }
+}
