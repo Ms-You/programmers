b775cf6fc9f9b89d18752bbe20d26fc9fc31c745
Myeongsu Yu <msu122005@hs.ac.kr>
2022-12-11 17:42:13 +0900

    가장 가까운 같은 글자 풀이

---
+++ Lv.1/가장 가까운 같은 글자/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        answer[0] = -1;
+        
+        for (int i = 1; i < s.length(); i++){
+            int val = s.substring(0, i).lastIndexOf(s.charAt(i));
+            if (val != -1) answer[i] = i-val;
+            else answer[i] = -1;
+        }
+        
+        return answer;
+    }
+}
