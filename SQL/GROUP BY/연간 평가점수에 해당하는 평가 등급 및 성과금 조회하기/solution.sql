f71e49ec00e8b143cab50fb8deb5cf1914a5dc4d
Ms-You <msu122005@hs.ac.kr>
2025-01-24 17:28:48 +0900

    sql - 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기

---
+++ SQL/GROUP BY/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/solution.sql
@@ -0,0 +1,55 @@
+-- 코드를 작성해주세요
+# SELECT 
+#     HE.EMP_NO,
+#     HE.EMP_NAME,
+#     CASE
+#         WHEN SUBQUERY.SCORE >= 96 THEN "S"
+#         WHEN SUBQUERY.SCORE >= 90 THEN "A"
+#         WHEN SUBQUERY.SCORE >= 80 THEN "B"
+#         ELSE "C"
+#     END AS GRADE,
+#     CASE 
+#         WHEN SUBQUERY.SCORE >= 96 THEN HE.SAL * 0.20
+#         WHEN SUBQUERY.SCORE >= 90 THEN HE.SAL * 0.15
+#         WHEN SUBQUERY.SCORE >= 80 THEN HE.SAL * 0.10
+#         ELSE 0
+#     END AS BONUS
+# FROM HR_EMPLOYEES HE
+# INNER JOIN (
+#     SELECT EMP_NO, AVG(SCORE) AS SCORE
+#     FROM HR_GRADE
+#     GROUP BY EMP_NO
+# ) AS SUBQUERY
+#     ON HE.EMP_NO = SUBQUERY.EMP_NO
+# GROUP BY HE.EMP_NO
+# ORDER BY EMP_NO ASC
+# ;
+# 또는
+-- 코드를 작성해주세요
+SELECT
+    HE.EMP_NO,
+    HE.EMP_NAME,
+    GRADE,
+    CASE 
+        WHEN GRADE = "S" THEN HE.SAL * 0.20
+        WHEN GRADE = "A" THEN HE.SAL * 0.15
+        WHEN GRADE = "B" THEN HE.SAL * 0.10
+        ELSE 0
+    END AS BONUS
+FROM HR_EMPLOYEES HE
+INNER JOIN (
+    SELECT
+        EMP_NO,
+        CASE
+            WHEN AVG(SCORE) >= 96 THEN "S"
+            WHEN AVG(SCORE) >= 90 THEN "A"
+            WHEN AVG(SCORE) >= 80 THEN "B"
+            ELSE "C"
+        END AS GRADE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+) SUB 
+    ON HE.EMP_NO = SUB.EMP_NO
+GROUP BY HE.EMP_NO
+ORDER BY HE.EMP_NO
+;
