c729ad832d432ac31b1c100ef1ab8bbfd862869f
Ms-You <msu122005@hs.ac.kr>
2025-02-06 16:10:50 +0900

    sql - Weather Observation Station 20

---
+++ HackerRank/SQL/Weather Observation Station 20/solution.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+*/
+SELECT ROUND(LAT_N, 4)
+FROM (
+    SELECT 
+        LAT_N, 
+        PERCENT_RANK() OVER(ORDER BY LAT_N) AS PERCENT
+    FROM STATION 
+) AS SUBQUERY
+WHERE PERCENT = 0.5
+;
