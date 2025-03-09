08e263465fd98a0247845ca3efa1627d6d9d70da
Ms-You <msu122005@hs.ac.kr>
2025-03-09 20:59:40 +0900

    Algorithms - Manasa and Stones

---
+++ HackerRank/Algorithms/Manasa and Stones/solution.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+    /*
+     * Complete the 'stones' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER a
+     *  3. INTEGER b
+     */
+
+    public static List<Integer> stones(int n, int a, int b) {
+    // Write your code here
+        List<Integer> result = new ArrayList<>();
+        
+        n -= 1; // 0
+        
+        int min = n * Math.min(a, b);
+        int max = n * Math.max(a, b);
+        int diff = Math.abs(a-b);
+        
+        result.add(min);
+        
+        int start = 0;
+        while (start + min < max) {
+            start += diff;
+            result.add(start+min);
+        }
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int a = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> result = Result.stones(n, a, b);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
