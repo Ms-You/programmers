49eaf451858dcacad2764dc21398ac1bd45f8ce4
Ms-You <msu122005@hs.ac.kr>
2025-03-19 00:36:51 +0900

    Algorithms - Gemstones

---
+++ HackerRank/Algorithms/Gemstones/solution.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gemstones' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY arr as parameter.
+     */
+
+    public static int gemstones(List<String> arr) {
+    // Write your code here
+        int result = 0;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        
+        for (char ch : alphabet.toCharArray()) {
+            boolean flag = true;
+            
+            for (String str : arr) {
+                if (!str.contains(String.valueOf(ch))) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) {
+                result++;
+            }
+        }
+        
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        int result = Result.gemstones(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
