52129b84fda76b79fdd3c3b911546d84665988c4
Ms-You <msu122005@hs.ac.kr>
2025-03-20 00:18:21 +0900

    Algorithms - Beautiful Binary String

---
+++ HackerRank/Algorithms/Beautiful Binary String/solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static int beautifulBinaryString(String b) {
+    // Write your code here
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder(b);
+        
+        for (int i = 0; i < sb.length()-2; i++) {
+            if (sb.substring(i, i+3).equals("010")) {
+                cnt++;
+                sb.setCharAt(i+2, '1');
+            }
+        }
+        
+        return cnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String b = bufferedReader.readLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
