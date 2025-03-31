4146a902ee0ba5473c9dd8ead0834b7643c34187
Ms-You <msu122005@hs.ac.kr>
2025-03-31 22:57:44 +0900

    Algorithms - Jim and the Orders

---
+++ HackerRank/Algorithms/Jim and the Orders/Solution.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jimOrders' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY orders as parameter.
+     */
+
+    public static List<Integer> jimOrders(List<List<Integer>> orders) {
+    // Write your code here
+        Map<Integer, Integer> orderMap = new HashMap<>();
+        
+        for (int i = 0; i < orders.size(); i++) {
+            orderMap.put(i+1, orders.get(i).get(0) + orders.get(i).get(1));
+        }
+        
+        List<Integer> keySet = new ArrayList<>(orderMap.keySet());
+                
+        Collections.sort(keySet, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return orderMap.get(o1) - orderMap.get(o2);
+            }
+        });
+        
+        return keySet;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> orders = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                orders.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.jimOrders(orders);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
